33ec7196d6e897112159b0265bc2b202be32445a
danilobml <danbarml@gmail.com>
2023-01-19 20:08:59 +0100

    enable create and modify user page in admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,7 +31,13 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.email)
 
     def test_modify_user_page_works(self):
-        url = reverse('admin:core_user_change', args=[self.user.id]),
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page_successful(self):
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
